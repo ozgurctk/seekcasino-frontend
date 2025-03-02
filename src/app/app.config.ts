cf67b59ead9a44b2440ed840eb599ee2a52f573b
ozgurc <ozg.ctkk@gmail.com>
2025-03-02 21:53:02 +0300

    app.config.ts eklendi

---
+++ src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    provideAnimations(),
+    importProvidersFrom(NgbModule)
+  ]
+};
